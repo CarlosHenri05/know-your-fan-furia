87a2d3c50cd61813c42275ee66f3c6f193e179ea
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-30 21:48:18 -0300

    Adding prompt and generate_profile method using Llama from meta

---
+++ llm.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+try:
+  client = Groq(api_key=GROQ_API_KEY)
+except Exception as e:
+  raise Exception("GROQ API KEY Not found.")
+
+
+def generate_profile(dados):
+  try:
+    prompt = f"""
+    Com base nos dados abaixo, escreva um perfil curto e personalizado de um fã de eSports da organização FURIA, especificamente FURIA:
+
+    Nome: {dados['nome']}
+    Nickname: {dados['nickname']}
+    Eventos: {dados['eventos']}
+    Memória favorita: {dados['memoria']}
+
+    Gere um texto com tom informal, mas informativo.
+    """
+
+    completion = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        messages= [{"role": "user", "content": prompt}],
+        temperature=0.7
+    )
+
+    return completion.choices[0].message.content.strip()
+
+  except Exception as e:
+    raise Exception(f"Algo deu errado. {e}")
+  
